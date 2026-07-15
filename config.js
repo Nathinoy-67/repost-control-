f10b35d8273b12b6bc88fb9907b61347b8aca94f
Nathinoy-67 <nathanguillerme1@gmail.com>
2026-07-15 17:59:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ const CONFIG = {
   GOOGLE_CLIENT_ID: "313624287408-5d78ccrvuhr4cprt46lpbs5t2rcmk0ur.apps.googleusercontent.com",
 
   // Meta for Developers > ton app > Paramètres de base > ID de l'app
-  FACEBOOK_APP_ID: "REMPLACE_MOI",
+  FACEBOOK_APP_ID: "1688605009069713",
 
   // URL de ton Worker Cloudflare (ex: https://repost-worker.tonpseudo.workers.dev)
-  WORKER_URL: "https://REMPLACE_MOI.workers.dev",
+  WORKER_URL: "https://repost-worker.nathanguillerme1.workers.dev",
 };
