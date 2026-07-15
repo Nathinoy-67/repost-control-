1b2338e7aec222a81c1d786171362d7dadc3806e
Nathinoy-67 <nathanguillerme1@gmail.com>
2026-07-15 19:15:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,15 @@
 // À REMPLIR — voir README.md pour savoir où récupérer chaque valeur
 // ==========================================================
 const CONFIG = {
-  // Google Cloud Console > Identifiants > OAuth client ID (type "Desktop app")
+  // Google Cloud Console > Identifiants > OAuth client ID (type "Web application")
   GOOGLE_CLIENT_ID: "313624287408-js44h1bb18jp6h5kcd80lijeejnrtdpc.apps.googleusercontent.com",
 
   // Meta for Developers > ton app > Paramètres de base > ID de l'app
+  FACEBOOK_APP_ID: "1688605009069713",
+
+  // Meta for Developers > Facebook Login for Business > Configurations > ta configuration
   FACEBOOK_CONFIG_ID: "2237919497004871",
 
-  // URL de ton Worker Cloudflare (ex: https://repost-worker.tonpseudo.workers.dev)
+  // URL de ton Worker Cloudflare
   WORKER_URL: "https://repost-worker.nathanguillerme1.workers.dev",
 };
